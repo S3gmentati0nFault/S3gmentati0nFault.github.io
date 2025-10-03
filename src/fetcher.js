90018f409ddf621271e79ca33fa4222e6548c98a
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2025-10-03 18:40:29 +0200

    Updated the fetcher, changed the structure of the query

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,18 +1,21 @@
 const fetch = require("node-fetch");
 
-export.handler = async(event) => {
+async function github() {
 	const token = process.env.GITHUB_KEY
 	const owner = process.env.OWNER
 	const repo = event.queryStringParameters.repo
 
 	const query = `
-		query {
-			repository(owner:$owner, name:$repo) {
-				object(expression:"HEAD:README.md") {
-		  		... on Blob { text }
-			}
-	      	}
-	}`;
+	  query {
+	    repository(owner: "$owner", name: "$repo") {
+	      object(expression: "main:README.md") {
+		... on Blob {
+		  text
+		}
+	      }
+	    }
+	  }
+	`;
 	
 	const r = await fetch(
 		"https://api.github.com/graphql",
@@ -20,15 +23,22 @@ export.handler = async(event) => {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `bearer ${token}`
+				Authorization: `Bearer $token`
 			},
-			body: JSON.stringify({ query, variables: { owner, name } })
+			body: JSON.stringify({ query })
 		}
 	);
 
 	const data = await r.json();
+	console.log(JSON.stringify(data))
+	console.log(r.status)
 	return {
-		statusCode: 200,
+		statusCode: r.status,
 		body: JSON.stringify(data)
 	};
 }
+
+
+
+
+github();
