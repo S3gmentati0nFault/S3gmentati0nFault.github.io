7a05029020db1dc75a2fe26f6600609e56449073
marvin@the-paranoid-android <alessandro.biagiotti@ziti.uni-heidelberg.de>
2025-11-09 18:34:02 +0100

    Changed the fish function to use the PythonUV package manager

--- setup.fish
+++ setup.fish
@@ -1,16 +1,20 @@
 #!/usr/bin/fish
 
+# Installing Python UV
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/share/../bin/env.fish
+
 # Check if there is a venv folder in the root folder of the project
-if not test -d "mkdocs_venv"
-	echo "Creating the virtual environment..."
-	python -m venv ./mkdocs_venv
+if not test -d mkdocs_venv
+    echo "Creating the virtual environment..."
+    uv venv ./mkdocs_venv
 end
 
 # Mount the virtual environment
 source ./mkdocs_venv/bin/activate.fish
 echo $VIRTUAL_ENV
 
-pip-compile requirements.in
+uv pip compile requirements.in --universal --output-file requirements.txt
 
 # Install the packages
-pip install -r requirements.txt
+uv pip sync requirements.txt
